7989c16e8f3d16b7d1165f2d7e6350902e4a9769
Ben Chidgey <ben@feedmypixel.com>
2024-03-12 16:51:31 +0000

    Update Nav test to include with and without appPathPrefix tests

--- src/config/nunjucks/context/build-navigation.test.js
+++ src/config/nunjucks/context/build-navigation.test.js
@@ -1,26 +1,84 @@
 import { buildNavigation } from '~/src/config/nunjucks/context/build-navigation'
+import { withPrefix } from '~/src/server/common/helpers/app-path-prefix/with-prefix'
 
-const mockRequest = ({ path = '' } = {}) => ({
-  path
+const mockRequest = ({ path = '', appPathPrefix } = {}) => ({
+  path,
+  withPrefix: (url) => withPrefix(url, appPathPrefix)
 })
 
 describe('#buildNavigation', () => {
-  test('Should provide expected navigation details', async () => {
-    expect(await buildNavigation(mockRequest())).toEqual([
-      {
-        isActive: false,
-        text: 'Home',
-        url: '/'
-      }
-    ])
+  describe('Without appPathPrefix', () => {
+    test('Should provide expected navigation details', () => {
+      expect(buildNavigation(mockRequest())).toEqual([
+        {
+          isActive: false,
+          text: 'Home',
+          url: '/'
+        },
+        {
+          isActive: false,
+          text: 'About',
+          url: '/about'
+        }
+      ])
+    })
+
+    test('Should provide expected highlighted navigation details', () => {
+      expect(buildNavigation(mockRequest({ path: '/' }))).toEqual([
+        {
+          isActive: true,
+          text: 'Home',
+          url: '/'
+        },
+        {
+          isActive: false,
+          text: 'About',
+          url: '/about'
+        }
+      ])
+    })
   })
-  test('Should provide expected highlighted navigation details', async () => {
-    expect(await buildNavigation(mockRequest({ path: '/' }))).toEqual([
-      {
-        isActive: true,
-        text: 'Home',
-        url: '/'
-      }
-    ])
+
+  describe('With appPathPrefix', () => {
+    test('Should provide expected navigation details', () => {
+      expect(
+        buildNavigation(
+          mockRequest({ appPathPrefix: '/cdp-node-frontend-template' })
+        )
+      ).toEqual([
+        {
+          isActive: false,
+          text: 'Home',
+          url: '/cdp-node-frontend-template'
+        },
+        {
+          isActive: false,
+          text: 'About',
+          url: '/cdp-node-frontend-template/about'
+        }
+      ])
+    })
+
+    test('Should provide expected highlighted navigation details', () => {
+      expect(
+        buildNavigation(
+          mockRequest({
+            path: '/cdp-node-frontend-template',
+            appPathPrefix: '/cdp-node-frontend-template'
+          })
+        )
+      ).toEqual([
+        {
+          isActive: true,
+          text: 'Home',
+          url: '/cdp-node-frontend-template'
+        },
+        {
+          isActive: false,
+          text: 'About',
+          url: '/cdp-node-frontend-template/about'
+        }
+      ])
+    })
   })
 })
