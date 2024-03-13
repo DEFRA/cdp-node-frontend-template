6a4e0064f9f663306c3b75b623084a0c1b7319fc
Ben Chidgey <ben@feedmypixel.com>
2024-03-13 08:47:08 +0000

    Add about page

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -2,16 +2,7 @@ const homeController = {
   handler: (request, h) => {
     return h.view('home/index', {
       pageTitle: 'Home',
-      heading: 'Home',
-      breadcrumbs: [
-        {
-          text: 'Home',
-          href: request.withPrefix('/')
-        },
-        {
-          text: 'Example'
-        }
-      ]
+      heading: 'Home'
     })
   }
 }
