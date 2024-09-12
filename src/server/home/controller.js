27110223c87af210e30bde555725594230180650
Ben Chidgey <ben@feedmypixel.com>
2024-09-12 14:18:14 +0100

    Mark request as unused

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -4,7 +4,7 @@
  * @satisfies {Partial<ServerRoute>}
  */
 export const homeController = {
-  handler(request, h) {
+  handler(_request, h) {
     return h.view('home/index', {
       pageTitle: 'Home',
       heading: 'Home'
