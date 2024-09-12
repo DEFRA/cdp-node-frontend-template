eae98a42a05a985df96e379e2ae0c764325e9a8f
Ben Chidgey <ben@feedmypixel.com>
2024-09-12 14:13:49 +0100

    Use return consistently

--- src/server/common/helpers/start-server.js
+++ src/server/common/helpers/start-server.js
@@ -4,20 +4,23 @@ import { createServer } from '~/src/server/index.js'
 import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
 
 async function startServer() {
+  let server
+
   try {
-    const server = await createServer()
+    server = await createServer()
     await server.start()
 
     server.logger.info('Server started successfully')
     server.logger.info(
       `Access your frontend on http://localhost:${config.get('port')}`
     )
-    return server
   } catch (error) {
     const logger = createLogger()
     logger.info('Server failed to start :(')
     logger.error(error)
   }
+
+  return server
 }
 
 export { startServer }
