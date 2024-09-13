2b3e3a1cccbc55f89cda9b74b0da46f524e361b0
Ben Chidgey <ben@feedmypixel.com>
2024-09-13 11:20:20 +0100

    Add basic controller tests

---
+++ src/server/about/controller.test.js
@@ -0,0 +1,32 @@
+import { createServer } from '~/src/server/index.js'
+import { statusCodes } from '~/src/server/common/constants/status-codes.js'
+
+describe('#aboutController', () => {
+  /** @type {Server} */
+  let server
+
+  beforeAll(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
+
+  afterAll(async () => {
+    await server.stop({ timeout: 0 })
+  })
+
+  test('Should provide expected response', async () => {
+    const { result, statusCode } = await server.inject({
+      method: 'GET',
+      url: '/about'
+    })
+
+    expect(result).toEqual(
+      expect.stringContaining('About | CDP Node.js Frontend Template')
+    )
+    expect(statusCode).toBe(statusCodes.ok)
+  })
+})
+
+/**
+ * @import { Server } from '@hapi/hapi'
+ */
---
+++ src/server/home/controller.test.js
@@ -0,0 +1,32 @@
+import { createServer } from '~/src/server/index.js'
+import { statusCodes } from '~/src/server/common/constants/status-codes.js'
+
+describe('#homeController', () => {
+  /** @type {Server} */
+  let server
+
+  beforeAll(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
+
+  afterAll(async () => {
+    await server.stop({ timeout: 0 })
+  })
+
+  test('Should provide expected response', async () => {
+    const { result, statusCode } = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+
+    expect(result).toEqual(
+      expect.stringContaining('Home | CDP Node.js Frontend Template')
+    )
+    expect(statusCode).toBe(statusCodes.ok)
+  })
+})
+
+/**
+ * @import { Server } from '@hapi/hapi'
+ */
