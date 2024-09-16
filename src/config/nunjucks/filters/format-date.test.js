acfb92f02ac58b0659327ae60c571b4e21e06874
Ben Chidgey <ben@feedmypixel.com>
2024-09-16 13:55:29 +0100

    Move date back before daylight saving

--- src/config/nunjucks/filters/format-date.test.js
+++ src/config/nunjucks/filters/format-date.test.js
@@ -3,7 +3,7 @@ import { formatDate } from '~/src/config/nunjucks/filters/format-date.js'
 describe('#formatDate', () => {
   beforeAll(() => {
     jest.useFakeTimers({
-      now: new Date('2023-04-01')
+      now: new Date('2023-02-01')
     })
   })
 
@@ -13,13 +13,15 @@ describe('#formatDate', () => {
 
   describe('With defaults', () => {
     test('Date should be in expected format', () => {
-      expect(formatDate('2023-04-08T11:40:02.242Z')).toBe('Sat 8th April 2023')
+      expect(formatDate('2023-02-01T11:40:02.242Z')).toBe(
+        'Wed 1st February 2023'
+      )
     })
   })
 
   describe('With Date object', () => {
     test('Date should be in expected format', () => {
-      expect(formatDate(new Date())).toBe('Sat 1st April 2023')
+      expect(formatDate(new Date())).toBe('Wed 1st February 2023')
     })
   })
 
@@ -27,10 +29,10 @@ describe('#formatDate', () => {
     test('Date should be in provided format', () => {
       expect(
         formatDate(
-          '2023-04-08T11:40:02.242Z',
+          '2023-02-01T11:40:02.242Z',
           "h:mm aaa 'on' EEEE do MMMM yyyy"
         )
-      ).toBe('12:40 pm on Saturday 8th April 2023')
+      ).toBe('11:40 am on Wednesday 1st February 2023')
     })
   })
 })
