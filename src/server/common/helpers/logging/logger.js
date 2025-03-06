29d64edd54e120be08d06f6271f41616eb5ddded
Phil Segal <phil@phisec.co.uk>
2025-03-06 15:43:41 +0000

    514789: Fix maxlisteners potential bug

--- src/server/common/helpers/logging/logger.js
+++ src/server/common/helpers/logging/logger.js
@@ -2,6 +2,10 @@ import { pino } from 'pino'
 
 import { loggerOptions } from '~/src/server/common/helpers/logging/logger-options.js'
 
-export function createLogger() {
-  return pino(loggerOptions)
+const logger = pino(loggerOptions)
+
+function createLogger() {
+  return logger
 }
+
+export { createLogger }
