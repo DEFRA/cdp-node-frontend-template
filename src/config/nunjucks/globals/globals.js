770af28c98ddd84c1c02d8427d86d839ac62ff86
Ben Chidgey <ben@feedmypixel.com>
2025-06-13 14:54:03 +0100

    CORE-680: move global file to a folder
    - Add govukRebrand global nunjucks flag, but turned off

---
+++ src/config/nunjucks/globals/globals.js
@@ -0,0 +1,4 @@
+// Turn on to enable GOV.UK rebrand - https://github.com/alphagov/govuk-frontend/blob/main/CHANGELOG.md#use-the-refreshed-govuk-brand-if-youre-using-our-nunjucks-page-template
+const govukRebrand = false
+
+export { govukRebrand }
