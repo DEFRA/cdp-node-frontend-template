6a9e2db50c925796bafb589ab9b659ee1e3dc53b
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-09-15 14:47:15 +0100

    CORE-964 removes example page content. tweaks csp

--- src/server/common/helpers/content-security-policy.js
+++ src/server/common/helpers/content-security-policy.js
@@ -7,16 +7,23 @@ import Blankie from 'blankie'
 const contentSecurityPolicy = {
   plugin: Blankie,
   options: {
+    // Hash 'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=' is to support a GOV.UK frontend script bundled within Nunjucks macros
+    // https://frontend.design-system.service.gov.uk/import-javascript/#if-our-inline-javascript-snippet-is-blocked-by-a-content-security-policy
     defaultSrc: ['self'],
     fontSrc: ['self', 'data:'],
     connectSrc: ['self', 'wss', 'data:'],
     mediaSrc: ['self'],
-    scriptSrc: ['self', 'data:', 'unsafe-inline'],
-    styleSrc: ['self', 'data:', 'unsafe-inline'],
+    styleSrc: ['self'],
+    scriptSrc: [
+      'self',
+      "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='"
+    ],
     imgSrc: ['self', 'data:'],
     frameSrc: ['self', 'data:'],
     objectSrc: ['none'],
     frameAncestors: ['none'],
+    formAction: ['self'],
+    manifestSrc: ['self'],
     generateNonces: false
   }
 }
