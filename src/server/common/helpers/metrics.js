e8130d30af3b3bd2e8145f6d1be2602a8bf6ccf5
chotai <chotai@users.noreply.github.com>
2026-01-28 10:50:03 +0000

    CORE-2277 Use @defra/cdp-metrics for metrics

--- src/server/common/helpers/metrics.js
+++ src/server/common/helpers/metrics.js
@@ -1,32 +1,15 @@
-import {
-  createMetricsLogger,
-  Unit,
-  StorageResolution
-} from 'aws-embedded-metrics'
-
-import { config } from '../../../config/config.js'
-import { createLogger } from './logging/logger.js'
-
 /**
  * Aws embedded metrics wrapper
+ * @param {Request} request
+ * @param {string} metricName
+ * @param {number} value
+ * @returns {Promise<void>}
  */
-export async function metricsCounter(metricName, value = 1) {
-  const isMetricsEnabled = config.get('isMetricsEnabled')
-
-  if (!isMetricsEnabled) {
-    return
-  }
-
-  try {
-    const metricsLogger = createMetricsLogger()
-    metricsLogger.putMetric(
-      metricName,
-      value,
-      Unit.Count,
-      StorageResolution.Standard
-    )
-    await metricsLogger.flush()
-  } catch (error) {
-    createLogger().error(error, error.message)
-  }
+export async function metricsCounter(request, metricName, value = 1) {
+  // decorated on server and request from "@defra/cdp-metrics"
+  // https://github.com/DEFRA/cdp-libraries/tree/main/packages/cdp-metrics
+  await request.metrics().counter('download-received', value)
+  // alternative if server and request decoration isn't desired
+  const metrics = new Metrics(request.logger)
+  metrics.counter('download-received', value)
 }
